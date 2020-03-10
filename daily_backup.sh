aefd23416300c0e57b20b65ee7eeec977c6ef1f9
Urs <44188442+albiurs@users.noreply.github.com>
2020-03-10 10:07:39 +0100

    Create new file

---
+++ daily_backup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+PATH=/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin
+
+
+
+########################
+### global variables ###
+########################
+
+backup_path='/absolute/path/to/backup';
+webroot_path='/absolute/path/to/webroot/e.g./public_html';
+
+
+
+##################
+# prepare backup #
+##################
+
+### update archive ###
+echo -e "\n\n\n\n\n\n\n\n\n\n UPDATE ARCHIVE \n";
+cd $backup_path/daily/;
+rm ./06/*;
+mv ./05/* ./06/;
+mv ./04/* ./05/;
+mv ./03/* ./04/;
+mv ./02/* ./03/;
+mv ./01/* ./02/;
+
+### Create backup log ###
+cd $backup_path/daily/01/;
+exec > ./backup.log 2>&1;
+
+### Empty Trash ###
+echo -e "\n\n\n\n\n\n\n\n\n\n EMPTY TRASH \n";
+cd $webroot_path/;
+# requires a directory "trash" within the webroot
+rm -Rf ./trash/*;
+
+echo -e "################\n# cd to backup directory #\n################\n";
+cd $backup_path/daily/;
+
+
+
+#################
+# create backup #
+#################
+
+echo -e "################\n# run backup_sites.sh #\n################\n";
+### run backup_sites.sh ###
+path/to/backup/scripts/backup_sites.sh $webroot_path;
+
+
+
+#######################
+# gzip and rename log #
+#######################
+
+echo -e "################\n# gzip and rename log #\n################\n";
+cd $backup_path/daily/01/;
+gzip ./backup.log;
+now=$(date +"%Y-%m-%d");
+mv ./backup.log.gz ./$now.backup.log.gz;
